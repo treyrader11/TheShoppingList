4c44177cb0ce499b557140b3842857deb8dcbb4a
Trey Rader <treyrdr09@gmail.com>
2016-07-22 20:42:19 -0500

    jquery added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,54 @@
+
+
+
 $(document).ready(function() {
-	$('.checked-list:first-child').click(function() { 
-   		$('#area').val('foobar');
+	$('.needed-list a').on("click", function() {
+		$(this).addClass('color-change');
+		
+	})
+	$('.needed-list a').on("dblclick", function() {
+		$(this).removeClass('color-change');
 	});
 });
+
+
+$('.btn').click(function() {
+  var userText = $('.add-item-input').val();
+  $('.needed-list').prepend("<li><a>" + userText + "</a></li>");
+  });
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//http://www.coding-issues.com/2013/10/how-to-setget-textbox-value-using.html
+//   http://stackoverflow.com/questions/2441565/how-do-i-make-a-div-element-editable-like-a-textarea-when-i-click-it
+//https://www.youtube.com/watch?v=_maaaWx3bYQ
+//https://www.youtube.com/watch?v=gIvR-WX5lIQ&list=PLsuLcuI9F2k1Hi0BQK3OE1wmkqiwG-dgd
+
+
+/*$(document).ready(function() {
+  $('.btn').on("click", function() {
+    var userText = $('.add-item-input').val();
+    $('.needed-list').append("<li><a>" + userText + "</a></li>");
+  })
+  $('.needed-list a').on("click", function() {
+    $(this).addClass('color-change');
+    $(this).on("dblclick", function() {
+      $(this).removeClass('color-change');
+    });
+  });
+
+});
+*/
+
+
