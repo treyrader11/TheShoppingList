7b04d5f1979f4d78301c0f01be6bf331a86dc66f
Trey Rader <treyrdr09@gmail.com>
2016-08-29 22:47:30 -0500

    refactored app/minimized code

--- js/script2.js
+++ js/script2.js
@@ -1,111 +1,82 @@
+'use strict'
 
 $(document).ready(function() {
-	$('.right-image').click(function() {
-		$(this).parent().remove();
+	//global variables
+	var $shoppingList = $('#needed-list');
+	var $item = $('li');
+	var $addItem = $('.add-item');
+	var $btn = $('#btn');
+	var $itemInput = $('#add-item-input');
+	var $arrow = $('#arrow-down');
+
+
+
+	$itemInput.blur();
+
+	$shoppingList.on('mouseenter', '.item', function() {
+		$(this).find('.right-icon').show();
+		$(this).find('.left-icon').show();
 	});
-	$('.left-image').on("click", function() {
-		$(this).parents('li').addClass('color-change');
+	$shoppingList.on('mouseleave', '.item', function() {
+		$(this).find('.left-icon').hide();
+		$(this).find('.right-icon').hide();
 	});
-	$('.left-image').dblclick(function() {
-		$(this).parents('li').removeClass('color-change')
-	})
 	
-	$('.left-image').hide();
-	$('.right-image').hide();
-	$('.needed-list li').mouseenter(function() {
-		$(this).addClass('selected');
-		$('.selected > .left-image').show(); 
-		$('.selected > .right-image').show();
+	$shoppingList.on('click', '.right-icon', function() {
+		$(this).closest('li').fadeOut(300, function() {
+			$(this).remove();
+		});
 	});
-	$('.needed-list li .selected').mouseleave(function() {
-		$(this).removeClass('selected')
-		$('.left-image').hide();
-		$('.right-image').hide()
+	$shoppingList.on('click', '.left-icon', function() {
+		$(this).closest('li').toggleClass('color-change');
 	});
 
 
-
-
-
-	$('.btn').click(function() {
-		var userInput = $('.add-item-input').val();
-		var newUserInput = "<li><img class='left-image' src='images/checkmark2.png' height='20' width='20'><a>" + userInput + "</a><img class='right-image' src='images/cut.jpg' height='20' width='20'></li>";
-		if(userInput == ' ') {
-				alert('PLease type an item into the text-box');
-		}
-		$('.needed-list').prepend(newUserInput);
-		$('.left-image').hide();
-		$('.right-image').hide();
-		$('.needed-list li').mouseenter(function() {
-			$('.left-image').show();
-			$('.right-image').show();
-		});
-		$('.needed-list li').mouseleave(function() {
-			$('.left-image').hide();
-			$('.right-image').hide()
-		});
-		$('.right-image').click(function() {
-			$(this).parent().remove();
-		});
-		$('.left-image').on("click", function() {
-			$(this).parents('li').addClass('color-change');
-		});
-		$('.left-image').dblclick(function() {
-			$(this).parents('li').removeClass('color-change')
-		})
-	});
-
-
-
-
-
-	$('.add-item').keydown(function (e) {
-		if (e.which == 13) {
-			var userInput = $('.add-item-input').val();
-			var newUserInput = "<li><img class='left-image' src='images/checkmark2.png' height='20' width='20'><a>" + userInput + "</a><img class='right-image' src='images/cut.jpg' height='20' width='20'></li>";
-			$('.needed-list').prepend(newUserInput);
-			$('.add-item-input').blur();
-			if(userInput == ' ') {
-				alert('PLease type an item into the text-box');
-				return false;
+	 $btn.click(function() {
+    	var inputValue = $itemInput.val();
+    	if(inputValue == ''){
+				alert("Please insert an item.");
 			}
-			$('.left-image').hide();	
-			$('.right-image').hide();
-			
-			$('.needed-list li').mouseenter(function() {
-				$('.left-image').show();
-				$('.right-image').show();
-			});
-			$('.needed-list li').mouseleave(function() {
-				$('.left-image').hide();
-				$('.right-image').hide();
-			});
-			$('.right-image').click(function() {
-				$(this).parent().remove();
-			});
-			$('.left-image').on("click", function() {
-				$(this).parents('li').addClass('color-change');
-			});
-			$('.left-image').dblclick(function() {
-				$(this).parents('li').removeClass('color-change')
-			});
-			$('.add-item-input').focus(function() {
-				$(this).val(" ");
-			}) 
+			else {
+				var newItem = "<li class='item'><img class='left-icon' src='images/checkmark2.png' height='20' width='20'><a>" + 
+					inputValue + 
+					"</a><img class='right-icon' src='images/cut.jpg' height='20' width='20'></li>";
+				$shoppingList.prepend(newItem);
+			}
+    	/*var newItem = "<li class='item'><img class='left-icon' src='images/checkmark2.png' height='20' width='20'><a>" + 
+			inputValue + 
+			"</a><img class='right-icon' src='images/cut.jpg' height='20' width='20'></li>";
+		$shoppingList.prepend(newItem);*/
+     });
+  
+
+	$addItem.keydown(function (e) {
+		if (e.which == 13) {
+			var inputValue = $itemInput.val();
+			if(inputValue == ''){
+				alert("Please insert an item.");
+			}
+			else {
+				var newItem = "<li class='item'><img class='left-icon' src='images/checkmark2.png' height='20' width='20'><a>" + 
+					inputValue + 
+					"</a><img class='right-icon' src='images/cut.jpg' height='20' width='20'></li>";
+				$shoppingList.prepend(newItem);
+			}
+			$itemInput.blur();
 			return false; //make sure this comes last for previous code to work	
-			 
-		}
-		
+		};
 	});
-	$('#arrow-down a').click(function() {
-		$('.needed-list').slideToggle("slow", function() {
 
-		});
+
+	$arrow.click(function() {
+		$('#needed-list').slideToggle("slow");
+	});
+	
+	$itemInput.focus(function() {
+		$(this).val('');
 	});
 });
 
-$(document).ready(function() {
-	$('.add-item-input').focus(function() {
-		$(this).val(" ");
-	}) 
-});
+
+
+
